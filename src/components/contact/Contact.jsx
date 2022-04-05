7261a85100ca349c4939a44dc2a02ead3e1027ff
4l3x <alexander.auermaa@gmail.com>
2022-04-06 00:30:34 +0200

    contact added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md' 
+import { RiMessengerLine } from 'react-icons/ri' 
+import { BsWhatsapp } from 'react-icons/bs' 
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>alexander.auermaa@gmail.com</h5>
+            <a href="mailto:alexander.auermaa@gmail.com" target="_blank">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Alexander Auermaa</h5>
+            <a href="https://m.me/auermaa" target="_blank">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+46723511583</h5>
+            <a href="https://api.whatsapp.com/send?phone=+46723511583" target="_blank">Go to WhatsApp</a>
+          </article>
+        </div>
+        <form action="">
+
+          <input type="text" name='name' placeholder='Your Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
